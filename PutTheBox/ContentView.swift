7095bd3bf6c90eb8b4c2e7c98ad85246189c1f31
Katopan <76249494+katomizuki@users.noreply.github.com>
2022-04-29 12:58:57 +0900

    add

--- PutTheBox/ContentView.swift
+++ PutTheBox/ContentView.swift
@@ -19,12 +19,41 @@ struct ARViewContainer: UIViewRepresentable {
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
+
+        let anchor = AnchorEntity()
+        anchor.position = simd_make_float3(0, -0.5, -1)
+        let box = ModelEntity(mesh: .generateBox(size: simd_make_float3(0.3, 0.1, 0.2), cornerRadius: 0.03))
+        var material = SimpleMaterial(color: .blue,
+                                      isMetallic: true)
+        material.metallic = 1
+        box.transform = Transform(pitch: 0, yaw: 1, roll: 0)
+        box.model?.materials = [material]
+        anchor.addChild(box)
+        arView.scene.anchors.append(anchor)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        let anchorEntity = AnchorEntity(world: [0, -1, -2])
+        let plane = ModelEntity(mesh: .generatePlane(width: 0.2, height: 0.3))
+        let planeMaterial = SimpleMaterial(color: .yellow, isMetallic: true)
+        let unlitMaterial = UnlitMaterial(color: .yellow)
+        plane.model?.materials = [unlitMaterial]
+        plane.transform = Transform(pitch: 0, yaw: 1, roll: 0)
+        anchorEntity.addChild(plane)
+        arView.scene.anchors.append(anchorEntity)
+ 
+        let spereEntity = AnchorEntity()
+        spereEntity.position = simd_make_float3(0, -0.5, -3)
+        let spererMaterial = SimpleMaterial(color: .purple, isMetallic: true)
+        let occulsionMaterial = OcclusionMaterial()
+        let spere = ModelEntity(mesh: .generateSphere(radius: 0.1), materials: [occulsionMaterial])
+        spereEntity.addChild(spere)
+        arView.scene.anchors.append(spereEntity)
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        let textAnchor = AnchorEntity()
+        textAnchor.position = simd_make_float3(0, -0.5, -1)
+        let text = ModelEntity(mesh: .generateText("Reality",font: .systemFont(ofSize: 0.1,weight: .bold),containerFrame: .zero, alignment: .center))
+        text.transform = Transform(pitch: 0, yaw: 0.3, roll: 0)
+        anchor.addChild(text)
+        arView.scene.anchors.append(anchor)
         
         return arView
         
